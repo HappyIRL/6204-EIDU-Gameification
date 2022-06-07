2da356987228bdf39a9312cc7057fa2c6478e186
Julian B <Jlbhappyirl@gmail.com>
2022-06-07 14:34:02 +0200

    Updated Splines

--- Assets/Scripts/ProgressionMap/ProgressionMap.cs
+++ Assets/Scripts/ProgressionMap/ProgressionMap.cs
@@ -1,48 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Transactions;
-using Redcode.Paths;
+using PathCreation;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ProgressionMap : MonoBehaviour
 {
-	[SerializeField] private Path path;
+	[SerializeField] private List<PathCreator> paths = new List<PathCreator>();
 	[SerializeField] private GameObject progressionIcon;
 	[SerializeField] private GameObject background;
 
-	[SerializeField] private List<float> steps = new List<float>();
-
-	private float currentDistance = 0f;
-
-	private int stepCounter = 0;
+	private int segmentIndex = 0;
 
 	public void NextStep(Action callback)
 	{
 		background.SetActive(true);
 		progressionIcon.SetActive(true);
-		StartCoroutine(MoveByDistanceOnPath(Mathf.Clamp(currentDistance + steps[stepCounter], 0f, 1f),2f, callback));
 
-		stepCounter++;
+		StartCoroutine(MoveByDistanceOnPath(paths[segmentIndex],1f, callback));
+		segmentIndex++;
 	}
 
-	private IEnumerator MoveByDistanceOnPath(float distance, float totalMoveTime, Action callback)
+	private IEnumerator MoveByDistanceOnPath(PathCreator pathCreator, float totalMoveTime, Action callback)
 	{
 		float time = 0;
-		PointData totalDistance = path.GetPointAtDistance(distance, true);
 
 		while (time < totalMoveTime)
 		{
 			time += Time.deltaTime;
-			float lerpT = time / totalMoveTime;
 
-			PointData currentPoint = path.GetPointAtDistance(currentDistance, true);
+			float percentileOfTotalTime = time / totalMoveTime;
 
-			progressionIcon.transform.position = Vector3.Lerp(currentPoint.Position, totalDistance.Position, lerpT);
+			Vector3 nextPoint = pathCreator.path.GetPointAtTime(percentileOfTotalTime, EndOfPathInstruction.Stop);
+
+			progressionIcon.transform.position = nextPoint;
+
 			yield return null;
 		}
-
-		progressionIcon.transform.position = totalDistance.Position;
 
 		yield return new WaitForSeconds(1f);
 		background.SetActive(false);
